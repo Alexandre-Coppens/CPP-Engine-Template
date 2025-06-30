b21f57ed76d8abcc03b8fcb4dd002692cfc9da9e
Alexandre-Coppens <alexandre.coppens@artfx.fr>
2025-06-30 16:06:21 +0200

    [Add] Open / Close Menu
    ADDED
    -The foundation of a menu that can be open / close.

--- Game Template/1-Map Creator/UI_TilesMenu.cpp
+++ Game Template/1-Map Creator/UI_TilesMenu.cpp
@@ -1,7 +1,8 @@
 #include "UI_TilesMenu.h"
 
 UI_TilesMenu::UI_TilesMenu(){
-	position = Vector2{ 30, GetScreenHeight() * 0.375f };
+	type = ActorType::UI;
+	position = Vector2{ 0, GetScreenHeight() * 0.375f };
 	size = Vector2{ 30, GetScreenHeight() * 0.250f };
 }
 
@@ -12,15 +13,18 @@ void UI_TilesMenu::Update(Vector2* scroll){
 }
 
 void UI_TilesMenu::Draw(Vector2* scroll){
-	Rectangle interactibleBar{ position.x - scroll->x, position.y - scroll->y, size.x, size.y };
+	Rectangle interactibleBar{ position.x, position.y, size.x, size.y };
+	Rectangle menuBackground{ 0, GetScreenHeight() - GetScreenHeight() * 0.95f, 300, GetScreenHeight() * 0.9f };
 	DrawRectanglePro(interactibleBar, Vector2Zero(), 0, BLACK);
 	if (open) {
-
+		DrawRectanglePro(menuBackground, Vector2Zero(), 0, LIGHTGRAY);
 	}
 }
 
 void  UI_TilesMenu::Clicked(){
 	open = !open;
+	if(open) position = position = Vector2{ 300, GetScreenHeight() * 0.375f };
+	else position = position = Vector2{ 0, GetScreenHeight() * 0.375f };
 }
 
 void UI_TilesMenu::OpenTilesTab(){
